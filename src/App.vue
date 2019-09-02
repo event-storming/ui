fe62480a7524db35a26385a43f8f6847cb5c6b32
kimsanghoon1 <sanghoon01@uengine.org>
2019-09-02 14:58:32 +0900

    navigation Menu

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,9 @@
       <!--&gt;</v-text-field>-->
       <div class="flex-grow-1"></div>
       <v-btn text @click="dialog = true">Login</v-btn>
-      <v-btn text>
-        <v-icon>perm_identity</v-icon>
-      </v-btn>
+      <!--<v-btn text>-->
+        <!--<v-icon>perm_identity</v-icon>-->
+      <!--</v-btn>-->
     </v-app-bar>
 
     <v-navigation-drawer
@@ -31,6 +31,32 @@
               dense
               class="grey lighten-4"
       >
+        <!-- User Info Start -->
+        <v-card
+                color="grey lighten-4"
+                class="mx-auto"
+                outlined
+        >
+          <v-list-item three-line>
+            <v-list-item-content>
+              <div class="overline mb-4">User Info</div>
+              <v-list-item-title class="headline mb-1">userName</v-list-item-title>
+              <v-list-item-subtitle>welcome</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-avatar
+                    tile
+                    size="80"
+                    color="grey"
+            ></v-list-item-avatar>
+
+          </v-list-item>
+        </v-card>
+        <!-- User Info End -->
+        <v-divider
+                dark
+                class="my-4"
+        ></v-divider>
         <template v-for="(item, i) in items">
           <v-row
                   v-if="item.heading"
@@ -41,15 +67,6 @@
               <v-subheader v-if="item.heading">
                 {{ item.heading }}
               </v-subheader>
-            </v-col>
-            <v-col
-                    cols="6"
-                    class="text-right"
-            >
-              <v-btn
-                      small
-                      text
-              >edit</v-btn>
             </v-col>
           </v-row>
           <v-divider
@@ -97,20 +114,14 @@
             drawer: null,
             dialog: false,
             items: [
-                { icon: 'touch_app', text: 'Reminders' },
-                { icon: 'lightbulb_outline', text: 'Notes' },
-                { divider: true },
-                { heading: 'Labels' },
-                { icon: 'add', text: 'Create new label' },
-                { divider: true },
-                { icon: 'archive', text: 'Archive' },
-                { icon: 'delete', text: 'Trash' },
-                { divider: true },
-                { icon: 'settings', text: 'Settings' },
-                { icon: 'chat_bubble', text: 'Trash' },
-                { icon: 'help', text: 'Help' },
-                { icon: 'phonelink', text: 'App downloads' },
-                { icon: 'keyboard', text: 'Keyboard shortcuts' },
+                // { heading: 'Labels' },
+                { icon: 'shopping_cart', text: 'Shop' },
+                // { icon: 'local_shipping', text: 'Delivery' },
+                { icon: 'perm_identity', text: 'My Page' },
+                // { divider: true },
+                // { heading: 'My Page' },
+                // { icon: 'local_shipping', text: 'Delivery' },
+                // { icon: 'shopping_cart', text: 'Shopping Cart' },
             ],
         }),
     }
