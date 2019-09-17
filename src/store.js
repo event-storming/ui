536ab2519c3a95b5cdd499027d50e64ff270bf77
lee-soo-heon <sooheon45@uengine.org>
2019-09-17 16:12:36 +0900

    aa

--- src/store.js
+++ src/store.js
@@ -22,6 +22,7 @@ export default new Vuex.Store({
             state.userId = userId;
             state.login = true;
             localStorage.setItem("accessToken", accessToken)
+            Vue.prototype.$http.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
             // 모든 HTTP 요청 헤더에 Authorization 을 추가한다.
             Vue.prototype.$http.get(`${window.API_HOST}/users/${state.userId}`).then(function (e) {
                 state.nickname = e.data.nickname
