d073c4bb5a64f4d5e5701e49dcdda1892b4addd9
lee-soo-heon <sooheon45@uengine.org>
2019-09-19 17:19:19 +0900

    aa

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,32 +1,32 @@
 <template>
-  <v-container>
-    <v-dialog
-            v-model="buyDialog"
-            width="800"
-    >
-      <order
-              v-if="buyDialog"
-              v-model="buy"
-              @cancel="buyDialog=false"
-      ></order>
-    </v-dialog>
+<!--  <v-container>-->
+<!--    <v-dialog-->
+<!--            v-model="buyDialog"-->
+<!--            width="800"-->
+<!--    >-->
+<!--      <order-->
+<!--              v-if="buyDialog"-->
+<!--              v-model="buy"-->
+<!--              @cancel="buyDialog=false"-->
+<!--      ></order>-->
+<!--    </v-dialog>-->
 
-    <v-dialog
-            v-model="editDialog"
-            width="500"
-    >
-      <product-add
-              v-if="editDialog"
-              v-model="edit"
-              @cancel="editDialog=false"
-      ></product-add>
-    </v-dialog>
+<!--    <v-dialog-->
+<!--            v-model="editDialog"-->
+<!--            width="500"-->
+<!--    >-->
+<!--      <product-add-->
+<!--              v-if="editDialog"-->
+<!--              v-model="edit"-->
+<!--              @cancel="editDialog=false"-->
+<!--      ></product-add>-->
+<!--    </v-dialog>-->
 
-    <product-list
-            @buyItem="buyValue"
-            @editItem="editValue"
-    ></product-list>
-  </v-container>
+<!--    <product-list-->
+<!--            @buyItem="buyValue"-->
+<!--            @editItem="editValue"-->
+<!--    ></product-list>-->
+<!--  </v-container>-->
 </template>
 <style>
   h1, h2 {
