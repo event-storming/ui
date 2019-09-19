ab72a44f3ce475398c4614582abe1353d64c8e0e
lee-soo-heon <sooheon45@uengine.org>
2019-09-19 16:13:58 +0900

    a

--- src/router.js
+++ src/router.js
@@ -17,7 +17,7 @@ export default new Router({
         {
             path: '/',
             name: 'home',
-            component: ProductPage,
+            component: Home,
         },
         {
             path: '/mypage',
