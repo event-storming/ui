8f26dea2b4c8dd1220667860383f2b823724da1c
Jinyoung Jang <jyjang@uengine.org>
2020-11-01 22:59:33 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,9 @@
 module.exports = {
     configureWebpack: {
         devServer: {
-            host: 'es2cd.io',
             port: '8080',
             disableHostCheck: true
         }
     },
-    "transpileDependencies": [
-        "vuetify"
-    ],
-    "runtimeCompiler": true
-    
 
 }
